63ce3c277cc5cb7b48cd4639c6cf1e1dbadcf447
unknown <akummer@uim-ws-srq-1765.UICNA.local>
2015-04-27 16:27:39 -0400

    changed shooting functions

--- game.js
+++ game.js
@@ -11,16 +11,9 @@ var mouseIsDown = false;
 var mousePosition = [];
 window.onmousedown = function(e){
     mouseIsDown = true;
-    player1.isShooting = true;
-    //player1.shoot(ctx);
-    //console.log('mouse down');
 }
 window.onmouseup = function(e){
     mouseIsDown = false;
-    player1.isShooting = false;
-
-    //player1.shoot(ctx);
-    //console.log('mouse up');
 }
 window.addEventListener("keydown", function(e) {
     keys[e.keyCode] = true;
@@ -40,19 +33,16 @@ window.addEventListener("mouseenter", function (e)
 {
     if(!started){
         started = true;
-        //console.log('mouse enter');
     }
 });
 canvas.addEventListener("mouseleave", function (e)
 {
     started = false;
-    //console.log('mouse exit');
 });
 window.addEventListener('mousemove', function(e)
 {
         var mousePos = getMousePos(canvas, e);
         mousePosition = getMousePos(canvas, e);
-        //console.log(e.clientX, e.clientY);
 }, false);
 
 
@@ -65,18 +55,16 @@ var lightningBolt = function(x,y,mx,my)
   this.my = my;
   this.update = function(ctx)
   {
-    //console.log(this.mx,this.my);
-    this.draw(ctx);
+
     this.x += 5;
     this.y += 5;
-
+    this.draw(ctx);
   };
   this.draw = function(ctx)
   {
     ctx.strokeStyle="yellow";
     ctx.rect(this.x,this.y,10,10);
     ctx.stroke();
-    //console.log("trying to draw lightning...");
   };
 }
 
@@ -94,21 +82,12 @@ var player = function(x,y,hearts,isShooting)
 
   this.shoot = function()
   {
-    if(this.isShooting)
-    {
-      var i = 0;
-      console.log("shooting");
-      bolts[i] = new lightningBolt(this.x+30,this.y+5,mousePosition.x,mousePosition.y);
-
-      i++;
-
-    }
-    else if(!this.isShooting)
-    {
-      console.log("not shooting");
-
-    }
-  }
+    var i = 0;
+    bolts[i] = new lightningBolt(this.x+30,this.y+5,mousePosition.x,mousePosition.y);
+    bolts.forEach(function(i) { i.update(ctx);})
+    console.log("shooting");
+    i++;
+  };
 };
 
 function update(mod)
@@ -129,21 +108,23 @@ function update(mod)
   {
     player1.y += 3;
 	}
-
+  if(mouseIsDown)
+  {
+    player1.shoot(ctx);
+  }
+  if(!mouseIsDown)
+  {
+    console.log("not shooting");
+  }
 }
 render = function()
 {
   ctx.fillStyle="black";
   ctx.fillRect(0,0,canvas.width,canvas.height);
   //ctx.clearRect(0,0,canvas.x,canvas.y);
-  bolts.forEach(function(i) { i.update(ctx);})
-
-
   update(null);
   player1.draw(ctx);
-  player1.shoot(ctx);
   requestAnimationFrame(render);
-
 }
 function main()
 {
